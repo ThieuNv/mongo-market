4b6bfb741d22b4fbf1b5fe335ee591c43354053a
ThieuNv <nguyenthieu2102@gmail.com>
2017-05-26 00:15:02 +0700

    Fix 1 mistake in detail-product.model

--- src/app/_models/product/detail-product.model.ts
+++ src/app/_models/product/detail-product.model.ts
@@ -2,7 +2,7 @@ import {Manufacturer} from './manufacturer.model';
 import {Price} from './price.model';
 
 export class DetailProduct {
-  constructor(public star: number,git
+  constructor(public star: number,
               public slogan: string,
               public description: string,
               public status: string, public inStock: number,
